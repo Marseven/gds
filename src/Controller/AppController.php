5529b826cba7d7dc9e5cccc1e1509017e66a64b8
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-01-16 03:44:53 +0100

    debug 1

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -94,6 +94,7 @@ class AppController extends Controller
         while (($emapData = fgetcsv($file, 10000, ";")) !== FALSE)
         {
             $count++;
+            var_dump($emapData[0]);die;
             if($emapData[0] == 'Matricule'){
                 $result = false;
             }else{
