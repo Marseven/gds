98d2a21dc3921e8b2c0c7bb6518c703ffb858d73
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-01-20 18:07:54 +0100

    debug 3

--- src/Controller/DeclarationsController.php
+++ src/Controller/DeclarationsController.php
@@ -742,8 +742,7 @@ class DeclarationsController extends AppController
             $finalTable = TableRegistry::get('final_datas');
             $configsTable = TableRegistry::get('configs');
             $config = $configsTable->find()->where(['id_user =' => $user['id']])->first();
-            debug($config);die;
-            if($config){
+            if($config == null){
                 $this->Flash->error('Veuillez configurer les informations de votre entreprise !');
                 return $this->redirect(['Controller' => 'Configs', 'action' => 'index']);
             }
